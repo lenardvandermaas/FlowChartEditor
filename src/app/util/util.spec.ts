82244e718b5aab96c7817ea50f2f5b94164165b4
Martijn Dirkse <martijn@integrationpartners.nl>
2024-04-01 11:03:06 +0200

    Have roundedMedian function

--- src/app/util/util.spec.ts
+++ src/app/util/util.spec.ts
@@ -1,4 +1,4 @@
-import { getRange } from './util'
+import { getRange, roundedMedian } from './util'
 
 describe('Util test', () => {
   it('Get range', () => {
@@ -21,5 +21,30 @@ describe('Util test', () => {
       caught = true
     }
     expect(caught).toBe(true)
+  })
+
+  it('Median of list of odd length', () => {
+    const input = [4, 3, 10]
+    expect(roundedMedian(input)).toBe(4)
+    // Check original items were not sorted
+    expect(input).toEqual([4, 3, 10])
+  })
+
+  it('Median of list of even length - no rounding needed', () => {
+    const input = [4, 3, 6, 10]
+    expect(roundedMedian(input)).toBe(5)
+  })
+
+  it('Median of list of even length - rounding needed', () => {
+    const input = [10, 100, 201, 500]
+    expect(roundedMedian(input)).toBe(150)
+  })
+
+  it('Median of singleton', () => {
+    expect(roundedMedian([3])).toBe(3)
+  })
+
+  it('Median of two elements', () => {
+    expect(roundedMedian([3, 5])).toBe(4)
   })
 })
--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -3,11 +3,28 @@
 // However, the syntax applied there does not look so clear.
 export function getRange(start: number, endNotIncluded: number): number[] {
   if (endNotIncluded < start) {
-	throw new Error(`Cannot generate range if end = ${endNotIncluded} < start = ${start}`)
+    throw new Error(`Cannot generate range if end = ${endNotIncluded} < start = ${start}`)
   }
   let result: number[] = []
   for(let i = start; i < endNotIncluded; ++i) {
-	result.push(i)
+    result.push(i)
   }
   return result
+}
+
+export function roundedMedian(arg: number[]): number {
+  if (arg.length == 0) {
+    throw new Error('Cannot calculate median of empty list')
+  }
+  // If no sort function is provided, numbers are sorted by their string representation
+  const argSorted = [ ... arg].sort((a, b) => a - b)
+  if (argSorted.length % 2 === 0) {
+    const lengthOfFirstHalf = argSorted.length / 2
+    const firstIndexForMedian = lengthOfFirstHalf - 1
+    return Math.floor( ( argSorted[firstIndexForMedian] + argSorted[firstIndexForMedian + 1] ) / 2)
+  } else {
+    const numItemsBeforeMid = (argSorted.length - 1) / 2
+    const mid = numItemsBeforeMid
+    return argSorted[mid]
+  }
 }
