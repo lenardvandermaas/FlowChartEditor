4b670003e906bbe45061ae645f82b17e57c08974
Martijn Dirkse <martijn@integrationpartners.nl>
2024-04-10 18:42:04 +0200

    Test calculating line crossings by integration test

--- src/app/components/flow-chart-editor/flow-chart-editor.component.ts
+++ src/app/components/flow-chart-editor/flow-chart-editor.component.ts
@@ -3,17 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { SequenceEditorComponent } from '../sequence-editor/sequence-editor.component';
 import { Drawing, FrankFlowchartComponent, Line, Rectangle, getEmptyDrawing } from '../frank-flowchart/frank-flowchart.component';
 import { getGraphFromMermaid } from '../../parsing/mermaid-parser';
-import { GraphBase, Graph, GraphConnectionsDecorator, ConcreteNode, Edge, getEdgeKey } from '../../model/graph';
-import { calculateLayerNumbers, CreationReason, NodeForEditor, NodeSequenceEditorBuilder, OriginalNode } from '../../model/horizontalGrouping';
+import { GraphBase, Graph, GraphConnectionsDecorator } from '../../model/graph';
+import { calculateLayerNumbers, CreationReason, NodeSequenceEditorBuilder } from '../../model/horizontalGrouping';
 import { NodeSequenceEditor } from '../../model/nodeSequenceEditor';
-import { Subject } from 'rxjs';
-import { NodeLayout, NodeLayoutBuilder, NodeSpacingDimensions, Position } from '../../graphics/node-layout';
-import { Interval } from '../../util/interval';
+import { NodeLayoutBuilder, NodeSpacingDimensions } from '../../graphics/node-layout';
 import { Layout, PlacedEdge, PlacedNode } from '../../graphics/edge-layout';
 
-interface Dimensions extends NodeSpacingDimensions {
+export interface Dimensions extends NodeSpacingDimensions {
   nodeBoxWidth: number
   nodeBoxHeight: number
+}
+
+export interface NodeSequenceEditorOrError {
+  model: NodeSequenceEditor | null
+  error: string | null
 }
 
 const dimensions: Dimensions = {
@@ -41,23 +44,29 @@ export class FlowChartEditorComponent {
   numCrossingLines: number = 0
 
   loadMermaid() {
+    const modelOrError: NodeSequenceEditorOrError = FlowChartEditorComponent.mermaid2model(this.mermaidText)
+    if (modelOrError.error !== null) {
+      alert(modelOrError.error)
+      return
+    }
+    this.layoutModel = modelOrError.model
+    this.updateDrawing()
+  }
+
+  static mermaid2model(text: string): NodeSequenceEditorOrError {
     let b: GraphBase
     try {
-      b = getGraphFromMermaid(this.mermaidText)
+      b = getGraphFromMermaid(text)
     } catch(e) {
-      alert('Invalid mermaid text:' + (e as Error).message)
-      return
+      return {model: null, error: 'Invalid mermaid text:' + (e as Error).message}
     }
     const g: Graph = new GraphConnectionsDecorator(b)
     const layerMap: Map<string, number> = calculateLayerNumbers(g)
     const builder: NodeSequenceEditorBuilder = new NodeSequenceEditorBuilder(layerMap, g)
     if (builder.orderedOmittedNodes.length > 0) {
-      alert('Could not assign a layer to the following nodes: ' + builder.orderedOmittedNodes.map(n => n.getId()).join(', '))
-      return
+      return {model: null, error: 'Could not assign a layer to the following nodes: ' + builder.orderedOmittedNodes.map(n => n.getId()).join(', ')}
     }
-    console.log('Pass model to SequenceEditorComponent')
-    this.layoutModel = builder.build()
-    this.updateDrawing()
+    return {model: builder.build(), error: null}
   }
 
   onSequenceEditorChanged() {
@@ -65,9 +74,7 @@ export class FlowChartEditorComponent {
   }
 
   updateDrawing() {
-    const builder = new NodeLayoutBuilder(this.layoutModel!, dimensions)
-    const nodeLayout = builder.run()
-    const layout = new Layout(nodeLayout, this.layoutModel!, dimensions)
+    const layout = FlowChartEditorComponent.model2layout(this.layoutModel!, dimensions)
     this.numCrossingLines = layout.getNumCrossingLines()
     const rectangles: Rectangle[] = layout.getNodes()
       .map(n => n as PlacedNode)
@@ -82,6 +89,12 @@ export class FlowChartEditorComponent {
         id: edge.key, x1: edge.line.startPoint.x, y1: edge.line.startPoint.y,
         x2: edge.line.endPoint.x, y2: edge.line.endPoint.y
       }})
-    this.drawing = {width: nodeLayout.width, height: nodeLayout.height, rectangles, lines}
+    this.drawing = {width: layout.width, height: layout.height, rectangles, lines}
+  }
+
+  static model2layout(model: NodeSequenceEditor, inDimensions: Dimensions): Layout {
+    const builder = new NodeLayoutBuilder(model, inDimensions)
+    const nodeLayout = builder.run()
+    return new Layout(nodeLayout, model, dimensions)
   }
 }
