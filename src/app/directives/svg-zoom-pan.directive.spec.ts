01ed10dddd6226c01d4ed724994078adf21ffd6a
lenardvandermaas <lenard@integrationpartners.nl>
2024-04-17 17:50:58 +0200

    fix directive test

--- src/app/directives/svg-zoom-pan.directive.spec.ts
+++ src/app/directives/svg-zoom-pan.directive.spec.ts
@@ -1,8 +1,31 @@
+import { By } from '@angular/platform-browser';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
 import { SvgZoomPanDirective } from './svg-zoom-pan.directive';
 
+@Component({
+  standalone: true,
+  template: `<svg [appSvgZoomPan]></svg>`,
+  imports: [SvgZoomPanDirective],
+})
+class TestComponent {}
+
 describe('SvgZoomPanDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let des: DebugElement[];
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      imports: [SvgZoomPanDirective, TestComponent],
+    }).createComponent(TestComponent);
+  
+    fixture.detectChanges(); // initial binding
+  
+    // all elements with an attached HighlightDirective
+    des = fixture.debugElement.queryAll(By.directive(SvgZoomPanDirective));
+  });
+
   it('should create an instance', () => {
-    const directive = new SvgZoomPanDirective();
-    expect(directive).toBeTruthy();
+    expect(des).toBeTruthy();
   });
 });
